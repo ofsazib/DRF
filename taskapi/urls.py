0d8c85306c4c8d621564c96a85a1787dcb94648e
Sazib <ofsazib@gmail.com>
2017-02-18 01:44:36 +0600

    add create contact view for post

--- taskapi/urls.py
+++ taskapi/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^api/v1/contact/$', views.contact_list, name = 'contact_list'),
+    url(r'^api/v1/contact/(?P<contact_id>[0-9])/$', views.contact_update_delete, name = 'contact_update_delete'),
 
 ]
--- taskapi/views.py
+++ taskapi/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Contact
+from rest_framework import status
+from taskapi.models import Contact
 from taskapi.serializers import ContactSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def contact_list(request):
     if request.method == 'GET':
         all_contact = Contact.objects.all()
         serializer = ContactSerializer(all_contact, many=True)
         return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
