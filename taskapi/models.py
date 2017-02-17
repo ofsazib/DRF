e039482d52b7276ee3009c62ce37d0159ef03fb8
Sazib <ofsazib@gmail.com>
2017-02-18 00:13:15 +0600

    create models

--- taskapi/models.py
+++ taskapi/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+''' models class for taskapi app'''
+class Contact(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=20)
+    phone = models.CharField(max_length=11)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
