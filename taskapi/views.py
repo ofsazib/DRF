aa9a00a8a0d0558ba34a33728b528cc81e3bd054
Sazib <ofsazib@gmail.com>
2017-02-18 00:41:49 +0600

    add contact_list view

--- taskapi/views.py
+++ taskapi/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Contact
+from taskapi.serializers import ContactSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def contact_list(request):
+    if request.method == 'GET':
+        all_contact = Contact.objects.all()
+        serializer = ContactSerializer(all_contact, many=True)
+        return Response(serializer.data)
